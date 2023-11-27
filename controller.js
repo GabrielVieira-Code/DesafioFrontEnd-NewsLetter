3406e2acd3b7b39e53f56e075af9ad74d43895b8
gabrielvieira-code <gabrielvieira.code@gmail.com>
2023-11-27 18:28:49 -0300

    correçao de bug da funçao de validaçao de email corrigida

--- controller.js
+++ controller.js
@@ -1,8 +1,10 @@
+let stateEmailValidation = true
 function emailvalidation(field) {
-    
+    if (stateEmailValidation==true) {
+        
         usuario = field.value.substring(0, field.value.indexOf("@"));
         dominio = field.value.substring(field.value.indexOf("@")+ 1, field.value.length);
-        // <p id="textEmail" class="textEmail">Email address</p>
+
         let textEmail =  document.getElementById("textEmail");
         let inputError = document.getElementById('username')
         var novaTagFilha = document.createElement('span');
@@ -17,36 +19,26 @@ function emailvalidation(field) {
             (dominio.lastIndexOf(".") < dominio.length - 1)) {
                 alert("E-mail valido");
         let inputElement=document.getElementById("userName");
+    }
+    else{
+        let novaTagFilha = document.createElement('span');
+        textEmail.appendChild(novaTagFilha);
         
-        }
-        else{
- console.log("foi",textEmail);
- let novaTagFilha = document.createElement('span');
-   textEmail.appendChild(novaTagFilha);
-//    inputError.id='teste'
-   novaTagFilha.textContent = 'Email invalido';
-   inputError.style.border = '1px solid red'
+        novaTagFilha.textContent = 'Email invalido';
+        inputError.style.border = '1px solid red'
+        
+        setTimeout(() => {
+            novaTagFilha.style.display = 'none'
+            inputError.style.border = 'rgb(181, 189, 196)'
+            console.log(inputError);
+        }, 1000);
+    }
+    stateEmailValidation = false
+    setTimeout(()=>{
+stateEmailValidation=true
+    },1500)
+    } 
 
-   setTimeout(() => {
-       novaTagFilha.style.display = 'none'
-      inputError.style.border = 'rgb(181, 189, 196)'
-        console.log(inputError);
-    }, 1000);
-        }
-       
-
-
-// // 1. Encontre a tag pai usando o ID
-// var tagPai = document.getElementById('pai');
-
-// // 2. Crie a nova tag filha
-// var novaTagFilha = document.createElement('span');
-
-// // 3. Adicione conteúdo à nova tag filha (opcional)
-// novaTagFilha.textContent = 'Esta é a tag filha.';
-
-// // 4. Adicione a nova tag filha à tag pai
-// tagPai.appendChild(novaTagFilha);
 
 
 
