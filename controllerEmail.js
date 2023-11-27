7c3f7acba9b9059659570f3ff161c6260a7ef25b
gabrielvieira-code <gabrielvieira.code@gmail.com>
2023-11-27 19:49:30 -0300

    create function senEmail

--- controllerEmail.js
+++ controllerEmail.js
@@ -1,1 +1,22 @@
-console.log("ola");
+const nodemailer = require('nodemailer');
+
+const transport =  nodemailer.createTransport({
+    host: 'smtp.office365.com',
+    port: 587,
+    secure: false,
+    auth:{
+        user:'socorro763testenode@outlook.com',
+        pass:'casaverde164',
+    }
+})
+transport.sendMail({
+    from:'gabriel <socorro763testenode@outlook.com>',
+    to:'gabrielvieira.code@gmail.com',
+    subject:'assunto',
+    html:'<p>foi que foi</p>',
+    text:'ola',
+})
+.then(()=>{
+    console.log('erro')
+}).catch((err)=>{console.log('erro ao enviar ',err)
+})
